d33d27a510365eee4c0302486c16095e4d0ad411
cevher.bozkur <bozkur@gmail.com>
2023-08-18 04:11:58 +0300

    Added UPC to domain BeerOrderLine
    Closes #2

--- src/main/java/guru/springframework/msscbeerorderservice/domain/BeerOrderLine.java
+++ src/main/java/guru/springframework/msscbeerorderservice/domain/BeerOrderLine.java
@@ -17,14 +17,16 @@ public class BeerOrderLine extends BaseEntity {
     private BeerOrder beerOrder;
 
     private UUID beerId;
+    private String upc;
     private Integer orderQuantity;
     private Integer quantityAllocated;
 
     @Builder
-    public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate, BeerOrder beerOrder, UUID beerId, Integer orderQuantity, Integer quantityAllocated) {
+    public BeerOrderLine(UUID id, Long version, Timestamp createdDate, Timestamp lastModifiedDate, BeerOrder beerOrder, UUID beerId, String upc, Integer orderQuantity, Integer quantityAllocated) {
         super(id, version, createdDate, lastModifiedDate);
         this.beerOrder = beerOrder;
         this.beerId = beerId;
+        this.upc = upc;
         this.orderQuantity = orderQuantity;
         this.quantityAllocated = quantityAllocated;
     }
